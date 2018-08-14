e035ffd0acf5671a582699c3a211b931d02cb0a6
F145h <its.f145h@gmail.com>
2018-08-14 22:21:28 +0300

    + files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+﻿var fs = require('fs');
+var path = require('path');
+var webpack = require('webpack');
+
+
+var nodeModules = {};
+fs.readdirSync('/node_modules')
+    .filter(function (x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+//nodeModules = {};
+
+module.exports = {
+    entry: './src/index.js',
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    externals: nodeModules,
+    output: {
+        path: path.resolve(__dirname, "distr"),
+        filename: 'node-generic-captcha'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
